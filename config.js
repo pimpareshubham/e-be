411aa4e566350fac1ddba2d6f8a7ecf22d4e04d1
pimpareshubham <127371609+pimpareshubham@users.noreply.github.com>
2023-11-25 12:52:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGODB_URL : mongodb+srv://shubhampimpare111:<password>@cluster0.zt66gfs.mongodb.net/?retryWrites=true&w=majority,
+    MONGODB_URL : "mongodb+srv://shubhampimpare111:<password>@cluster0.zt66gfs.mongodb.net/?retryWrites=true&w=majority",
     JWT_SECRET : "jajsndsjndksdnkasamdn"
 }
