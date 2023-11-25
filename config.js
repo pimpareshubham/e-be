e6d0ddaf306664211d927b8c78b296f892b479e6
pimpareshubham <127371609+pimpareshubham@users.noreply.github.com>
2023-11-25 15:54:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     MONGODB_URL : mongodb+srv://vercel-admin-user:73wqCKf5fHPjgmV1@cluster0.seqxqng.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    MONGODB_URI : mongodb+srv://vercel-admin-user:73wqCKf5fHPjgmV1@cluster0.seqxqng.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     JWT_SECRET : "jajsndsjndksdnkasamdn"
 }
